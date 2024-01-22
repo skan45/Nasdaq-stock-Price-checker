90b83748ffcf2f26430c5659e44311ab7e513f70
skan45 <gritliskander42@gmail.com>
2024-01-22 18:41:20 +0100

    hello

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 require("./db_connection");//database connection
 const app = express();
-
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc:["'self'","https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"],
+    styleSrc: ["'self'"]
+  }
+}));
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,10 +32,10 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -39,12 +44,12 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,89 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
 
+    test("viewing get request /api/stock-prices/", function (done) {
+        chai
+            .request(server)
+            .get("/api/stock-prices/")
+            .set("content-type", "application/json")
+            .query({ stock: "TSLA" })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.stockData.stock, "TSLA")
+                assert.exists(res.body.stockData.price, "TSLA  price")
+                done()
+            });
+
+    });
+    test("viewing one stock and liking it : GET request to /api/stock-price/?like=true", function (done) {
+        chai
+            .request(server)
+            .get("/api/stock-prices/")
+            .set("content-type", "application/json")
+            .query({ stock: "GOLD", like: true })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.stockData.stock, "GOLD")
+                assert.equal(res.body.stockData.likes, 1)
+                assert.exists(res.body.stockData.price, "GOLD  price")
+                done();
+            }
+            )
+    });
+    test("viewing the same  stock and liking it again  : GET request to /api/stock-price/?like=true", function (done) {
+        chai
+            .request(server)
+            .get("/api/stock-prices/")
+            .set("content-type", "application/json")
+            .query({ stock: "GOLD", like: true })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.stockData.stock, "GOLD")
+                assert.equal(res.body.stockData.likes, 1)
+                assert.exists(res.body.stockData.price, "GOLD  price")
+                done();
+            }
+            )
+    });
+    test("viewing two stocks: GET request to /api/stock-price/", function (done) {
+        chai
+            .request(server)
+            .get("/api/stock-prices/")
+            .set("content-type", "application/json")
+            .query({ stock: ["AMZN", "T"] })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.stockData[0].stock, "AMZN");
+                assert.equal(res.body.stockData[1].stock, "T");
+                assert.exists(res.body.stockData[0].prices, "AMZN has price");
+                assert.exists(res.body.stockData[1].prices, "T has price");
+                done();
+            });
+    });
+    test("viewing two stocks: GET request to /api/stock-price/ and liking them", function (done) {
+    chai
+        .request(server)
+        .get("/api/stock-prices/")
+        .set("content-type", "application/json")
+        .query({ stock: ["AMZN", "T"], like: true })
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.stockData[0].stock, "AMZN");
+            assert.equal(res.body.stockData[1].stock, "T");
+            assert.exists(res.body.stockData[0].prices, "AMZN has price");
+            assert.exists(res.body.stockData[1].prices, "T has price");
+            assert.exists(res.body.stockData[0].rel_likes, "AMZN has rel_likes");
+            assert.exists(res.body.stockData[1].rel_likes, "T has rel_likes");
+            done();
+        });
 });
+
+    
+
+
+
+
+})
+
